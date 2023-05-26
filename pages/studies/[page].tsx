c475e4147240fbfeb9c0e4c4985348455c549ce1
neo1415 <adedaniel502@gmail.com>
2023-05-26 08:22:28 +0100

    page

--- pages/studies/[page].tsx
+++ pages/studies/[page].tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, GetStaticPaths } from 'next';
+// import { GetServerSideProps, GetStaticPaths } from 'next';
 // import Studies, { getStaticProps as sharedGetStaticProps } from '../studies';
 
 // export default Studies;
@@ -8,14 +8,14 @@ import { GetServerSideProps, GetStaticPaths } from 'next';
 //   return func(ctx);
 // };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const studiesReq = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/studies`);
-  const studiesData = await studiesReq.json();
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const studiesReq = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/studies`);
+//   const studiesData = await studiesReq.json();
 
-  return {
-    paths: Array.from(Array(studiesData.totalPages)).map((page, i) => ({
-      params: { page: (i + 1).toString() },
-    })),
-    fallback: false,
-  };
-};
+//   return {
+//     paths: Array.from(Array(studiesData.totalPages)).map((page, i) => ({
+//       params: { page: (i + 1).toString() },
+//     })),
+//     fallback: false,
+//   };
+// };
