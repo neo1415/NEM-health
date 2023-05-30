de847400bcf01321a6d5c2ea5e1940fb72910bf2
neo1415 <adedaniel502@gmail.com>
2023-05-30 10:58:54 +0100

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,6 @@ import { TransitionGroup, CSSTransition, SwitchTransition } from 'react-transiti
 import { GridContainerWidthProvider } from '../components/layout/GridContainer';
 import Header from '../components/layout/Header';
 import useStyles from '../css/app';
-// import '../css/scss';
 import zIndex from '../css/zIndex';
 import breakpoints from '../css/breakpoints';
 import { base } from '../css/base';
@@ -66,13 +65,13 @@ const MyApp = (appProps: AppProps): React.ReactElement => {
   return (
     <ScrollInfoProvider>
       <WindowInfoProvider
-        breakpoints={{
-          xs: `(max-width: ${breakpoints.xs}px)`,
-          s: `(max-width: ${breakpoints.s}px)`,
-          m: `(max-width: ${breakpoints.m}px)`,
-          l: `(max-width: ${breakpoints.l}px)`,
-          xl: `(max-width: ${breakpoints.xl}px)`,
-        }}
+              breakpoints={{
+                xs: `(max-width: ${breakpoints.xs}px)`,
+                s: `(max-width: ${breakpoints.s}px)`,
+                m: `(max-width: ${breakpoints.m}px)`,
+                l: `(max-width: ${breakpoints.l}px)`,
+                xl: `(max-width: ${breakpoints.xl}px)`,
+              }}
       >
         <ModalProvider
           zIndex={zIndex.modal}
